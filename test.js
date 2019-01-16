367a8d11cdb441bba6812a26d8b93e1d090e13ad
tiepmiyatsu <39144702+tiepmiyatsu@users.noreply.github.com>
2019-01-16 10:42:36 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-(function ()
-{
-alert("Hi");
-}());
+$(function() {
+  $.ajax({ 
+    dataType:'jsonp', 
+    url: 'https://jasons-experiments.myshopify.com/cart.json?callback=?', 
+    success: function(d){ 
+      /* do something with the data. In this example we are just loggin it */
+      console.log(d) 
+    } 
+  });
+});
