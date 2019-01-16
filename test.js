5e575d47e37f596b11ea366184fb931a2f112681
tiepmiyatsu <39144702+tiepmiyatsu@users.noreply.github.com>
2019-01-16 10:47:09 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-$(function() {
-  $.ajax({ 
-    dataType:'jsonp', 
-    url: 'https://jasons-experiments.myshopify.com/cart.json?callback=?', 
-    success: function(d){ 
-      /* do something with the data. In this example we are just loggin it */
-      console.log(d) 
-    } 
-  });
-});
+function getCartData(data){
+  /* do something with the data. In this example we are just loggin it */
+  console.log(data)
+}
+
+var script = document.createElement('script');
+script.src = 'https://jasons-experiments.myshopify.com/cart.json?callback=getCartData'
+document.getElementsByTagName('head')[0].appendChild(script);
